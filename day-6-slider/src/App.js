1873d1e2dd3e82729bb53bcc31c460b079a2e1df
Fábio Matias <fabio_mcm2@hotmail.com>
2021-12-14 23:15:27 +0000

    day-6: solution

--- day-6-slider/src/App.js
+++ day-6-slider/src/App.js
@@ -1,23 +1,30 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+
+  const onChange = React.useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="amount">
+        <sup>$</sup>
+        <span className="dollars">{Number(value).toFixed(2)}</span>
+      </div>
+      <input
+        type="range"
+        id="priceRange"
+        min="0"
+        max="10000"
+        step="0.01"
+        value={value}
+        onChange={onChange}
+      />
+      <br />
+      <button>Buy Now</button>
     </div>
   );
 }
